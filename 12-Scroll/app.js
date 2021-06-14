37412fc113d79435798fc9d64e9b341d17f983ed
RizaHariati <rizahariati@yahoo.com>
2021-06-14 17:37:11 +0700

    Scroll exercise part 2 from coding Addict

--- 12-Scroll/app.js
+++ 12-Scroll/app.js
@@ -1,16 +1,61 @@
-const toggleBtn = document.querySelector(".nav-icon");
-const navContainer = document.querySelector(".nav-container");
+const date = document.querySelector(".date");
+const toggleBtn = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+const scrollLinks = document.querySelectorAll(".scroll-link");
+const nav = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+const yearToday = new Date();
+date.textContent = yearToday.getFullYear();
 
 toggleBtn.addEventListener("click", () => {
-  navContainer.classList.toggle("show");
+  linksContainer.classList.toggle("show-links");
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
 });
 
-window.addEventListener("scroll", () => {
-  const scrollHeight = window.pageYOffset;
-  const navHeight = navContainer.getBoundingClientRect().height;
-  if (scrollHeight > navHeight) {
-    navContainer.classList.add("scroll");
+window.addEventListener("scroll", (e) => {
+  const heightWindow = window.pageYOffset;
+  const heightNavbar = nav.getBoundingClientRect().height;
+  if (heightWindow > heightNavbar) {
+    nav.classList.add("fixed-nav");
   } else {
-    navContainer.classList.remove("scroll");
+    nav.classList.remove("fixed-nav");
   }
+  if (heightWindow > 400) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+const titles = document.querySelectorAll(".titles");
+scrollLinks.forEach((scrollLink) => {
+  scrollLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    const linkClicked = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(linkClicked);
+    const navHeight = nav.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = nav.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
 });
